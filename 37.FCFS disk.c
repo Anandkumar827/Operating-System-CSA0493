5b2fe9219086e808d1bf06674f10acc1919d9e74
Anandkumar827 <mungandikumar0076.sse@saveetha.com>
2023-01-28 14:20:48 +0530

    Update 37.FCFS disk.c

--- 37.FCFS disk.c
+++ 37.FCFS disk.c
@@ -8,27 +8,22 @@ int main() {
     int head, total_distance = 0;
     int i;
 
-    // Input the number of disk requests
     printf("Enter the number of disk requests: ");
     scanf("%d", &num_requests);
 
-    // Input the disk requests
     printf("Enter the disk requests:\n");
     for(i = 0; i < num_requests; i++) {
         scanf("%d", &requests[i]);
     }
 
-    // Input the initial position of the disk head
     printf("Enter the initial position of the disk head: ");
     scanf("%d", &head);
 
-    // First Come First Served algorithm
     for(i = 0; i < num_requests; i++) {
         total_distance += abs(head - requests[i]);
         head = requests[i];
     }
 
-    // Output the total distance
     printf("Total distance: %d\n", total_distance);
 
     return 0;
